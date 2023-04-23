6f80aa3f62fccfcadc2a8605739f1bc57470787d
Ignasiya <ignasiya93@gmail.com>
2023-04-23 09:39:30 +0300

    Ho,ework

--- 13.HomeWork_5/task_26.py
+++ 13.HomeWork_5/task_26.py
@@ -6,13 +6,15 @@ numbers = list(map(int, input('Enter a value A and B: ').split()))
 num_a = numbers[0]
 num_b = numbers[1]
 
+
 def PowNumber(a, b):
     if b == 0:
         return 1
     elif b < 0:
-        return 1 / (PowNumber(a, -b - 1) * a)
+        return 1 / PowNumber(a, b + 1) * 1 / a
     else:
         return PowNumber(a, b - 1) * a
+
 
 print(f"The number A = {num_a} "
       f"to the integer power B = {num_b} "
